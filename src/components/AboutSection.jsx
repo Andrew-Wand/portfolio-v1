ec742e982c2507ddf669efecafdcb37ca1dad414
Andrew Wand <andrewwands@gmail.com>
2023-03-18 20:10:21 -0500

    added css for skills seciton with icons

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,26 +1,32 @@
 import PortfolioPic from "../assets/portfolio-pic.jpg";
+import { HiCode } from "react-icons/hi";
 
 function AboutSection() {
   return (
-    <div className="h-[800px] bg-[#111827] flex justify-center ">
-      <div className=" w-[20rem] mt-14">
-        <h2 className="text-lg mt-14 text-left text-white">About</h2>
-        <br />
-        <p className="text-left text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
-          veniam quidem quaerat! Consequuntur natus atque odio aut consequatur
-          neque? Cumque voluptatum architecto quaerat accusamus autem officiis
-          adipisci blanditiis, soluta eum?
-        </p>
-        <figure>
-          <img
-            src={PortfolioPic}
-            alt="Picture of Andrew"
-            className="rounded-full mt-10 shadow-xl"
-          />
-        </figure>
+    <section>
+      <div className="h-[800px] bg-[#111827] flex justify-center">
+        <div className=" w-[20rem] mt-14">
+          <h2 className="text-lg mt-14 text-left text-white">About</h2>
+          <br />
+          <p className="text-left text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+            veniam quidem quaerat! Consequuntur natus atque odio aut consequatur
+            neque? Cumque voluptatum architecto quaerat accusamus autem officiis
+            adipisci blanditiis, soluta eum?
+          </p>
+          <figure>
+            <img
+              src={PortfolioPic}
+              alt="Picture of Andrew"
+              className="rounded-full mt-10 shadow-xl"
+            />
+          </figure>
+        </div>
       </div>
-    </div>
+      <div className="divider m-0 px-6 bg-[#111827] text-[4rem] mt-10">
+        <HiCode />
+      </div>
+    </section>
   );
 }
 
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,11 +1,79 @@
+import { FaCogs } from "react-icons/fa";
+import { ImMobile } from "react-icons/im";
+import { BiUniversalAccess } from "react-icons/bi";
+import { HiCode } from "react-icons/hi";
+
 function SkillsSection() {
   return (
-    <div className="h-[600px] bg-[#111827] flex justify-center">
-      <div className="w-[20rem] mt-14">
-        <h3 className="text-lg mt-14 text-left text-white">Skills</h3>
-        <div></div>
+    <section>
+      <div className="h-[1200px] bg-[#111827] flex justify-center">
+        <div className="w-[20rem] mt-14">
+          <h3 className="text-lg mt-14 text-left text-white mb-20">Skills</h3>
+          <div className="grid grid-cols-2 gap-16 mt-10 text-center">
+            <div>
+              <i class="devicon-html5-plain text-[5rem] text-[#e54d26]"></i>
+              <p className="text-xl mt-5">HTML5</p>
+            </div>
+            <div>
+              <i class="devicon-css3-plain text-[5rem] text-[#3d8fc6]"></i>
+              <p className="text-xl mt-5">CSS3</p>
+            </div>
+            <div>
+              <i class="devicon-javascript-plain colored text-[5rem] text-[#f0db4f]"></i>
+              <p className="text-xl mt-5">JavaScript</p>
+            </div>
+            <div>
+              <i class="devicon-react-original colored text-[5rem] text-[#61dafb]"></i>
+              <p className="text-xl mt-5">React</p>
+            </div>
+
+            <div>
+              <i class="devicon-git-plain colored text-[5rem] text-[#f05033]"></i>
+              <p className="text-xl mt-5">Git</p>
+            </div>
+
+            <div>
+              <i class="devicon-github-original text-[5rem] text-white"></i>
+              <p className="text-xl mt-5">GitHub</p>
+            </div>
+
+            <div>
+              <i class="devicon-firebase-plain text-[5rem] text-[#ffcc30]"></i>
+              <p className="text-xl mt-5">Firebase</p>
+            </div>
+
+            <div>
+              <div className="ml-5">
+                <i class="text-[5rem] text-[#f7e7ce]">
+                  <FaCogs />
+                </i>
+              </div>
+              <p className="text-xl mt-5">RESTful APIs</p>
+            </div>
+
+            <div>
+              <div className="ml-7">
+                <i class="text-[5rem] text-[#f5f5dc]">
+                  <ImMobile />
+                </i>
+              </div>
+              <p className="text-xl mt-5 ">Responsive Websites</p>
+            </div>
+            <div>
+              <div className="ml-7">
+                <i class="text-[5rem] text-white">
+                  <BiUniversalAccess />
+                </i>
+              </div>
+              <p className="text-xl mt-5">Accessibility</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="divider m-0 px-6 bg-[#111827] text-[4rem]">
+        <HiCode />
+      </div>
+    </section>
   );
 }
 
