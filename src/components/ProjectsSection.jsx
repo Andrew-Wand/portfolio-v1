55e65bb589a149695b713ffddfb8b1de87e47d35
Andrew Wand <andrewwands@gmail.com>
2023-03-21 12:03:51 -0500

    added project info to project section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,11 +1,205 @@
-import React from "react";
+import ShoppingAppScreenShot from "../assets/shoppingapp-screenshot.jpg";
 
 function ProjectsSection() {
   return (
     <section>
-      <div className="h-[1200px] bg-[#111827] flex justify-center">
+      <div className=" bg-[#111827] flex justify-center">
         <div className="w-[20rem] mt-14">
           <h2 className="text-lg mt-14 text-left text-white">Projects</h2>
+
+          {/* SHOPPING APP */}
+          <div className="mt-20">
+            <div>
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg"
+                src={ShoppingAppScreenShot}
+                alt="Shopping App Image"
+              />
+            </div>
+
+            <div className="mt-5">
+              <h4 className="mb-10 text-xl text-white">Clothing Store App</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+            </div>
+            <div className="mt-5 ">
+              <ul className="grid grid-cols-3">
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>CSS3</li>
+                <li>Firebase</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+
+            <div className="mt-5 flex justify-center">
+              <a
+                className="btn mr-5"
+                href="https://shopping-app-react-z3nr.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Project Here
+              </a>
+              <a
+                className="btn"
+                href="https://github.com/Andrew-Wand/shopping-app-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Repo Here
+              </a>
+            </div>
+          </div>
+          {/* NEXT APP */}
+
+          <div className="mt-20">
+            <div>
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg"
+                src={ShoppingAppScreenShot}
+                alt="Shopping App Image"
+              />
+            </div>
+
+            <div className="mt-5">
+              <h4 className="mb-10 text-xl text-white">Clothing Store App</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+            </div>
+            <div className="mt-5 ">
+              <ul className="grid grid-cols-3">
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>CSS3</li>
+                <li>Firebase</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+
+            <div className="mt-5 flex justify-center">
+              <a
+                className="btn mr-5"
+                href="https://shopping-app-react-z3nr.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Project Here
+              </a>
+              <a
+                className="btn"
+                href="https://github.com/Andrew-Wand/shopping-app-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Repo Here
+              </a>
+            </div>
+          </div>
+          {/* NEXT APP */}
+          <div className="mt-20">
+            <div>
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg"
+                src={ShoppingAppScreenShot}
+                alt="Shopping App Image"
+              />
+            </div>
+
+            <div className="mt-5">
+              <h4 className="mb-10 text-xl text-white">Clothing Store App</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+            </div>
+            <div className="mt-5 ">
+              <ul className="grid grid-cols-3">
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>CSS3</li>
+                <li>Firebase</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+
+            <div className="mt-5 flex justify-center">
+              <a
+                className="btn mr-5"
+                href="https://shopping-app-react-z3nr.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Project Here
+              </a>
+              <a
+                className="btn"
+                href="https://github.com/Andrew-Wand/shopping-app-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Repo Here
+              </a>
+            </div>
+          </div>
+          {/* NEXT APP */}
+          <div className="mt-20 mb-10">
+            <div>
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg"
+                src={ShoppingAppScreenShot}
+                alt="Shopping App Image"
+              />
+            </div>
+
+            <div className="mt-5">
+              <h4 className="mb-10 text-xl text-white">Clothing Store App</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+            </div>
+            <div className="mt-5 ">
+              <ul className="grid grid-cols-3">
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>CSS3</li>
+                <li>Firebase</li>
+                <li>Tailwind CSS</li>
+              </ul>
+            </div>
+
+            <div className="mt-5 flex justify-center">
+              <a
+                className="btn mr-5"
+                href="https://shopping-app-react-z3nr.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Project Here
+              </a>
+              <a
+                className="btn"
+                href="https://github.com/Andrew-Wand/shopping-app-react"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Repo Here
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
