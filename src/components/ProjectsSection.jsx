bcd4d57fede8d7e98d24b0adde2f67abd65bfce3
Andrew-Wand <andrewwands@gmail.com>
2023-06-07 14:00:02 -0500

    testing for vercel

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -18,9 +18,114 @@ function ProjectsSection() {
             Projects
           </h2>
 
-          {/* SHOPPING APP */}
+        
           <div
             className="mt-28 lg:flex lg:justify-between lg:flex-nowrap lg:flex-row-reverse lg:my-[10rem]"
+            data-aos="fade-up"
+          >
+            {/* <div className="">
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg lg:h-full"
+                src={shoppingappscreenshot}
+                alt="Shopping App Image"
+              />
+            </div> */}
+
+            <div className="mt-5 lg:mr-7">
+              <h4 className="mb-10 text-xl text-white lg:text-3xl">
+                Clothing Store App
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+              <div className="mt-5 ">
+                <ul className="grid grid-cols-2 text-white">
+                  <li>⬥ React</li>
+                  <li>⬥ JavaScript</li>
+                  <li>⬥ Firebase</li>
+                  <li>⬥ TailwindCSS</li>
+                </ul>
+              </div>
+
+              <div className="mt-5 flex justify-center">
+                <a
+                  className="btn mr-5"
+                  href="https://shopping-app-react-z3nr.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Project Here
+                </a>
+                <a
+                  className="btn"
+                  href="https://github.com/Andrew-Wand/shopping-app-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Repo Here
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* NEXT APP */}
+          <div
+            className="mt-20 lg:flex lg:justify-between lg:flex-nowrap lg:my-[14rem]"
+            data-aos="fade-up"
+          >
+            <div className="">
+              <img
+                className="h-[200px] rounded-xl drop-shadow-lg lg:h-full"
+                src={shoppingappscreenshot}
+                alt="Shopping App Image"
+              />
+            </div>
+            <div className="mt-5 lg:ml-7">
+              <h4 className="mb-10 text-xl text-white lg:text-3xl">
+                Clothing Store App
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                quis repellat necessitatibus debitis dolorum cum impedit sed
+                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
+                iure velit iusto reiciendis culpa quaerat.
+              </p>
+              <div className="mt-5 ">
+                <ul className="grid grid-cols-2 text-white">
+                  <li>⬥ React</li>
+                  <li>⬥ JavaScript</li>
+                  <li>⬥ Firebase</li>
+                  <li>⬥ TailwindCSS</li>
+                </ul>
+              </div>
+
+              <div className="mt-5 flex justify-center">
+                <a
+                  className="btn mr-5"
+                  href="https://shopping-app-react-z3nr.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Project Here
+                </a>
+                <a
+                  className="btn"
+                  href="https://github.com/Andrew-Wand/shopping-app-react"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Repo Here
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* NEXT APP */}
+          <div
+            className="mt-20 lg:flex lg:justify-between lg:flex-nowrap lg:flex-row-reverse lg:my-[14rem]"
             data-aos="fade-up"
           >
             <div className="">
@@ -73,118 +178,13 @@ function ProjectsSection() {
 
           {/* NEXT APP */}
           <div
-            className="mt-20 lg:flex lg:justify-between lg:flex-nowrap lg:my-[14rem]"
-            data-aos="fade-up"
-          >
-            <div className="">
-              <img
-                className="h-[200px] rounded-xl drop-shadow-lg lg:h-full"
-                src={ShoppingAppScreenShot}
-                alt="Shopping App Image"
-              />
-            </div>
-            <div className="mt-5 lg:ml-7">
-              <h4 className="mb-10 text-xl text-white lg:text-3xl">
-                Clothing Store App
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                quis repellat necessitatibus debitis dolorum cum impedit sed
-                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
-                iure velit iusto reiciendis culpa quaerat.
-              </p>
-              <div className="mt-5 ">
-                <ul className="grid grid-cols-2 text-white">
-                  <li>⬥ React</li>
-                  <li>⬥ JavaScript</li>
-                  <li>⬥ Firebase</li>
-                  <li>⬥ TailwindCSS</li>
-                </ul>
-              </div>
-
-              <div className="mt-5 flex justify-center">
-                <a
-                  className="btn mr-5"
-                  href="https://shopping-app-react-z3nr.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Project Here
-                </a>
-                <a
-                  className="btn"
-                  href="https://github.com/Andrew-Wand/shopping-app-react"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Repo Here
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* NEXT APP */}
-          <div
-            className="mt-20 lg:flex lg:justify-between lg:flex-nowrap lg:flex-row-reverse lg:my-[14rem]"
-            data-aos="fade-up"
-          >
-            <div className="">
-              <img
-                className="h-[200px] rounded-xl drop-shadow-lg lg:h-full"
-                src={ShoppingAppScreenShot}
-                alt="Shopping App Image"
-              />
-            </div>
-
-            <div className="mt-5 lg:mr-7">
-              <h4 className="mb-10 text-xl text-white lg:text-3xl">
-                Clothing Store App
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                quis repellat necessitatibus debitis dolorum cum impedit sed
-                laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
-                iure velit iusto reiciendis culpa quaerat.
-              </p>
-              <div className="mt-5 ">
-                <ul className="grid grid-cols-2 text-white">
-                  <li>⬥ React</li>
-                  <li>⬥ JavaScript</li>
-                  <li>⬥ Firebase</li>
-                  <li>⬥ TailwindCSS</li>
-                </ul>
-              </div>
-
-              <div className="mt-5 flex justify-center">
-                <a
-                  className="btn mr-5"
-                  href="https://shopping-app-react-z3nr.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Project Here
-                </a>
-                <a
-                  className="btn"
-                  href="https://github.com/Andrew-Wand/shopping-app-react"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View Repo Here
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* NEXT APP */}
-          <div
             className="mt-20 mb-14 lg:flex lg:justify-between lg:flex-nowrap  lg:my-[14rem]"
             data-aos="fade-up"
           >
             <div className="">
               <img
                 className="h-[200px] rounded-xl drop-shadow-lg lg:h-full"
-                src={ShoppingAppScreenShot}
+                src={shoppingappscreenshot}
                 alt="Shopping App Image"
               />
             </div>
