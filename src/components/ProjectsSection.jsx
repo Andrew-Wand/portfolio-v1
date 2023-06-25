6dd225bf084ece49cb0eab8595699985d73391f4
Andrew-Wand <andrewwands@gmail.com>
2023-06-25 16:01:09 -0500

    Added pet finder app to projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import ShoppingAppPic from '../assets/shopping-app-pic.webp'
-import MovieNightPic from '../assets/movie-night-pic.webp'
-import NetflixClonePic from '../assets/netflix-clone-pic.webp'
-import HangmanGamePic from '../assets/hangman-game-pic.webp'
+import ShoppingAppPic from "../assets/shopping-app-pic.webp";
+import MovieNightPic from "../assets/movie-night-pic.webp";
+import NetflixClonePic from "../assets/netflix-clone-pic.webp";
+import HangmanGamePic from "../assets/hangman-game-pic.webp";
+import PetFinderPic from "../assets/pet-finder-pic.webp";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { BiLinkExternal } from "react-icons/bi";
@@ -22,7 +23,7 @@ function ProjectsSection() {
             Projects
           </h2>
 
-        {/* First app */}
+          {/* First app */}
           <div
             className="mt-28 lg:flex lg:justify-between lg:flex-nowrap lg:flex-row-reverse lg:my-[10rem]"
             data-aos="fade-up"
@@ -40,7 +41,9 @@ function ProjectsSection() {
                 The Top Shop
               </h3>
               <p>
-               Clothing shop website specializing in shirts and other types of tops. Create an account, log in, and add things to your wishlist. When ready, you can then go to your cart to check out.
+                Clothing shop website specializing in shirts and other types of
+                tops. Create an account, log in, and add things to your
+                wishlist. When ready, you can then go to your cart to check out.
               </p>
               <div className="mt-5 ">
                 <ul className="grid grid-cols-2 text-white">
@@ -59,7 +62,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open link to new tab to view The Top Shop website."
                 >
-                  View Project Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Project Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
                 <a
                   className="btn"
@@ -68,7 +74,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open link to new tab to view repo for The Top Shop project."
                 >
-                  View Repo Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Repo Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
               </div>
             </div>
@@ -91,7 +100,11 @@ function ProjectsSection() {
                 Movie Night
               </h3>
               <p>
-                A movie rating app, created because I love watching movies, and I wanted a way to save and rate the movies I had watched. Just create an account and add your movies to the list. When you finish watching a movie, you can rate it and add the date watched.
+                A movie rating app, created because I love watching movies, and
+                I wanted a way to save and rate the movies I had watched. Just
+                create an account and add your movies to the list. When you
+                finish watching a movie, you can rate it and add the date
+                watched.
               </p>
               <div className="mt-5 ">
                 <ul className="grid grid-cols-2 text-white">
@@ -111,7 +124,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open link to new tab to view the Movie Night website."
                 >
-                  View Project Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Project Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
                 <a
                   className="btn"
@@ -120,7 +136,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open link to new tab to view the Movie Night repo."
                 >
-                  View Repo Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Repo Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
               </div>
             </div>
@@ -144,7 +163,7 @@ function ProjectsSection() {
                 Hangman Game
               </h3>
               <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
                 quis repellat necessitatibus debitis dolorum cum impedit sed
                 laudantium cumque quibusdam, dignissimos quasi ipsam dolorem
                 iure velit iusto reiciendis culpa quaerat.
@@ -154,7 +173,6 @@ function ProjectsSection() {
                   <li>⬥ HTML5</li>
                   <li>⬥ JavaScript</li>
                   <li>⬥ CSS3</li>
-               
                 </ul>
               </div>
 
@@ -166,7 +184,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open a new tab to view the Hangman Game website."
                 >
-                  View Project Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Project Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
                 <a
                   className="btn"
@@ -175,7 +196,10 @@ function ProjectsSection() {
                   rel="noopener noreferrer"
                   aria-label="open a new tab to view the Hangman Game repo."
                 >
-                  View Repo Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Repo Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
               </div>
             </div>
@@ -189,13 +213,13 @@ function ProjectsSection() {
             <div className="">
               <img
                 className="h-[200px] w-[350px] rounded-xl drop-shadow-lg lg:h-auto lg:w-full lg:mt-5"
-                src={NetflixClonePic}
+                src={PetFinderPic}
                 alt="Netflix Clone Image"
               />
             </div>
             <div className="mt-5 lg:ml-7">
               <h3 className="mb-10 text-xl text-white lg:text-3xl">
-                Netflix Clone
+                Pet Finder API App
               </h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -208,27 +232,34 @@ function ProjectsSection() {
                   <li>⬥ React</li>
                   <li>⬥ HTML5</li>
                   <li>⬥ CSS3</li>
+                  <li>⬥ RESTful API</li>
                 </ul>
               </div>
 
               <div className="mt-5 flex justify-center">
                 <a
                   className="btn mr-5"
-                  href="https://shopping-app-react-z3nr.vercel.app/"
+                  href="https://pet-finder-app-one.vercel.app/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label="open a new tab to view the Netflix Clone website."
+                  aria-label="open a new tab to view the Pet Finder website."
                 >
-                  View Project Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Project Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
                 <a
                   className="btn"
-                  href="https://github.com/Andrew-Wand/shopping-app-react"
+                  href="https://github.com/Andrew-Wand/pet-finder-app"
                   target="_blank"
                   rel="noopener noreferrer"
-                  aria-label="open a new tab to view the Netflix Clone repo."
+                  aria-label="open a new tab to view the Pet Finder repo."
                 >
-                  View Repo Here <span className="ml-3"><BiLinkExternal /></span>
+                  View Repo Here{" "}
+                  <span className="ml-3">
+                    <BiLinkExternal />
+                  </span>
                 </a>
               </div>
             </div>
